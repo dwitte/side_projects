8f0476c29e2a8cf14a6b54f8d0a97a4e6b7fd074
Derek Witte <dwitte@mines.edu>
2014-08-24 18:34:57 -0600

    Got a kernel to work with the grub bootloader.

--- DWOS/tutorials/Bare_Bones/kernel.c
+++ DWOS/tutorials/Bare_Bones/kernel.c
@@ -1,15 +1,120 @@
-#include <stdint-gcc.h>
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
 
-void kmain(void)
+enum vga_color
 {
-	extern uint32_t magic;
+	COLOR_BLACK = 0,
+	COLOR_BLUE = 1,
+	COLOR_GREEN = 2,
+	COLOR_CYAN = 3,
+	COLOR_RED = 4, 
+	COLOR_MAGENTA = 5,
+	COLOR_BROWN = 6,
+	COLOR_LIGHT_GREY = 7,
+};
 
-	if ( magic != 0x2BADB002 )
+enum function_returns
+{
+	FAIL = 0,
+	PASS = 1,
+};
+
+uint8_t make_color(enum vga_color fg, enum vga_color bg)
+{
+	return fg | bg << 4;
+}
+
+uint16_t make_vgaentry(char c, uint8_t color)
+{
+	uint16_t c16 = c;
+	uint16_t color16 = color;
+	return c16 | color16 << 8;
+}
+
+size_t strlen(const char* str)
+{
+	size_t ret = 0;
+	while ( str[ret] != 0 )
+		ret++;
+	return ret;
+}
+
+static const size_t VGA_WIDTH = 80;
+static const size_t VGA_HEIGHT = 25;
+
+size_t terminal_row;
+size_t terminal_column;
+uint8_t terminal_color;
+uint16_t* terminal_buffer;
+
+void terminal_initialize()
+{
+	terminal_row = 0;
+	terminal_column = 0;
+	terminal_color = make_color(COLOR_LIGHT_GREY, COLOR_BLACK);
+	terminal_buffer = (uint16_t*) 0xB8000;
+	for ( size_t y =0; y < VGA_HEIGHT; y++ )
 	{
-		
+		for ( size_t x = 0; x < VGA_WIDTH; x++ )
+		{
+			const size_t index = y * VGA_WIDTH + x;
+			terminal_buffer[index] = make_vgaentry(' ', terminal_color);
+		}
 	}
+}
 
-	volatile unsigned char *videoram = (unsigned char *)0xB8000;
-	videoram[0] = 65;
-	videoram[1] = 0x07;
+void terminal_setcolor(uint8_t color)
+{
+	terminal_color = color;
+}
+
+void terminal_putentryat( char c, uint8_t color, size_t x, size_t y)
+{
+	const size_t index = y * VGA_WIDTH + x;
+	terminal_buffer[index] = make_vgaentry(c, color);
+}
+
+uint8_t terminal_handlespecialchars( char c )
+{
+	uint8_t return_val = FAIL;
+
+	if ( c == '\n' )
+	{
+		terminal_row++;
+		terminal_column = 0;
+		return_val = PASS;
+	}	
+
+	return ( return_val );	
+}
+
+void terminal_putchar(char c )
+{
+	if ( terminal_handlespecialchars( c ) == FAIL )
+	{
+		terminal_putentryat(c, terminal_color, terminal_column, terminal_row);
+		if ( ++terminal_column == VGA_WIDTH )
+		{
+			terminal_column = 0;
+			if ( ++terminal_row == VGA_HEIGHT )
+			{
+				terminal_row = 0;
+			}
+		}
+	}
+}
+
+void terminal_writestring(const char* data)
+{
+	size_t datalen = strlen(data);
+	for ( size_t i = 0; i < datalen; i++ )
+		terminal_putchar(data[i]);
+}
+
+void kernel_main(void)
+{
+	terminal_initialize();
+	terminal_writestring("Welcome to the Kernel!\n");
+	terminal_writestring("Loading the rest of the OS...\n");
 }
--- DWOS/tutorials/src/bootloader.asm
+++ DWOS/tutorials/src/bootloader.asm
@@ -10,7 +10,22 @@ ORG 0x7C00
 ;Code Section (440 Bytes)
 jmp 0x0000:start
 start:
+	
 	jmp start
+
+ExtendedRead:		; dl needs to be set by the caller. (This is the Drive number)
+					; Also a packet needs setup such that the si register points to
+					;	Offset    | Size  | Description
+					;    0x00     | BYTE  | SIze of packet (0x10 or 0x18)
+					;    0x01     | BYTE  | Reserved (Must be 0x00)
+					;    0x02	  | WORD  | Number of blocks to transfer
+					;    0x04     | DWORD | -> Transfer Buffer
+					;    0x08     | QWORD | Starting Absolute Block Number
+					;	 0x10     | QWORD | 64-bit flat address of transfer buffer (used if DWROD at 0x04 is 0xFFFF:0xFFFF
+	mov ah, 0x42    ; This is an extended read command
+
+	
+	
 
 times 440-($-$$) db 0 ;Fill the rest of the code section with 0x00
 
@@ -21,6 +36,8 @@ times 4 db 0 ;Fill extra space with 0x00
 times 2 db 0	; Fill location with 0x00
 
 ;Partition Table (64 Bytes)
+;Empty until a need arises to utilize different partitions.
+;Currently only loading from a floppy disk image. 
 
 times 64 db 0
 
